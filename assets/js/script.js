abef5a67d618e21252141bfc0e0d08e6870781a0
Ramiz Manked <ramiz.manked@multidots.com>
2025-02-04 12:37:39 +0530

    Redirect from docs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,4 @@
 (function () {
     'use strict';
     alert('Welcome to GitHub Pages!');
-
-    const currentPath = window.location.pathname;
-    const pathToRedirect = "/docs";  // Change this to your desired path
-    const newURL = "https://example.com";     // Change this to your desired URL
-
-    if (currentPath === pathToRedirect) {
-        window.location.href = newURL;
-    }
 })();
